a04faeae402116ea1cff45ff9b47771867e009ac
William Tang <wilm.tang@outlook.com>
2017-10-30 02:41:01 -0700

    Adding first time visit remaining step judge. This can be proved wrong mathematically.

--- SAP.java
+++ SAP.java
@@ -1,133 +1,185 @@
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.Queue;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-
+import java.util.Queue;
 
 public class SAP {
+
     private Digraph graph;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
+        if (G == null) {
+            throw new IllegalArgumentException();
+        }
         graph = new Digraph(G);
+    }
+
+    private void validIndex(int i) {
+        if (i < 0 || i >= graph.V()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        ArrayList<Integer> i1 = new ArrayList<>();
-        i1.add(v);
-        ArrayList<Integer> i2 = new ArrayList<>();
-        i2.add(w);
-        return length(i1, i2);
+        validIndex(v);
+        validIndex(w);
+
+        ArrayList<Integer> tmp1 = new ArrayList<>();
+        tmp1.add(v);
+        ArrayList<Integer> tmp2 = new ArrayList<>();
+        tmp2.add(w);
+
+        return length(tmp1, tmp2);
     }
 
-    // a common ancestor of v and w that participates in a shortest ancestral
-    // path; -1 if no such path
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        ArrayList<Integer> i1 = new ArrayList<>();
-        i1.add(v);
-        ArrayList<Integer> i2 = new ArrayList<>();
-        i2.add(w);
-        return ancestor(i1, i2);
+        validIndex(v);
+        validIndex(w);
+
+        ArrayList<Integer> tmp1 = new ArrayList<>();
+        tmp1.add(v);
+        ArrayList<Integer> tmp2 = new ArrayList<>();
+        tmp2.add(w);
+        return ancestor(tmp1, tmp2);
     }
 
-    // length of shortest ancestral path between any vertex in v and any vertex
-    // in w; -1 if no such path
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        boolean[] marked1 = new boolean[graph.V()];
-        int[] length = new int[graph.V()];
-        bfs1(v, marked1, length);
-        boolean[] marked2 = new boolean[graph.V()];
-        int ret = bfs2(w, marked1, length, marked2)[0];
-        // System.out.println(Arrays.toString(marked1));
-        // System.out.println(Arrays.toString(marked2));
-        return ret;
+        if (v == null || w == null) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Integer i : v) {
+            validIndex(i);
+        }
+        for (Integer i : w) {
+            validIndex(i);
+        }
+
+        return bfs(v, w)[0];
     }
 
-    // a common ancestor that participates in shortest ancestral path; -1 if no
-    // such path
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        boolean[] marked1 = new boolean[graph.V()];
-        int[] length = new int[graph.V()];
-        bfs1(v, marked1, length);
-        boolean[] marked2 = new boolean[graph.V()];
-        int ret = bfs2(w, marked1, length, marked2)[1];
-        return ret;
+        if (v == null || w == null) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Integer i : v) {
+            validIndex(i);
+        }
+        for (Integer i : w) {
+            validIndex(i);
+        }
+
+        return bfs(v, w)[1];
     }
 
     /**
-     * use bfs to mark all the vertexes and the distance from the original
-     * vertex
+     * Using bidirectional bfs search
      *
-     * @param v         current vertex
-     * @param marked    represents the state of the vertex
-     * @param length    distance from the original vertex
-     * @param curLength current length
+     * @return first element if length, second is ancestor.
      */
-    private void bfs1(Iterable<Integer> itr, boolean[] marked, int[] length) {
-        Queue<Integer> q = new Queue<Integer>();
-        for (int v : itr) {
-            q.enqueue(v);
-            marked[v] = true;
-            length[v] = 0;
-        }
-        while (!q.isEmpty()) {
-            int cur = q.dequeue();
-//            System.out.println("bfs1 " + cur + " " + length[cur]);
-            for (int w : graph.adj(cur)) {
-                if (!marked[w]) {
-                    q.enqueue(w);
-                    marked[w] = true;
-                    length[w] = length[cur] + 1;
-                }
-            }
-        }
-    }
+    private int[] bfs(Iterable<Integer> v, Iterable<Integer> w) {
+        Queue<Integer> queue1 = new LinkedList<>();
+        Queue<Integer> queue2 = new LinkedList<>();
 
-    /**
-     * find the shortest ancestral path
-     *
-     * @param w         vertex w
-     * @param marked1   represents the vertexes bfs1 marked
-     * @param length    represents the corresponding distance bfs1 marked
-     * @param marked2   represents the vertexes bfs2 marked
-     * @param curLength current distance from original vertex
-     * @return a pair of integer, int[0] is the distance, int[1] is the common
-     * ancestor
-     */
-    private int[] bfs2(Iterable<Integer> itr, boolean[] marked1, int[] length, boolean[] marked2) {
-        Queue<Integer> q = new Queue<Integer>();
-        int[] distTo = new int[graph.V()];
-        for (int w : itr) {
-            q.enqueue(w);
-            marked2[w] = true;
-            distTo[w] = 0;
+        // Maps from vertex to smallest depth bfs from starting points.
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+        HashMap<Integer, Integer> map2 = new HashMap<>();
+
+        for (int i : v) {
+            queue1.add(i);
         }
-        int distance = -1;
-        int ancestor = -1;
-        while (!q.isEmpty()) {
-            int cur = q.dequeue();
-//            System.out.println("bfs2 " + cur + " " + distTo[cur]);
-            if (marked1[cur]) {
-                int dist = length[cur] + distTo[cur];
-                if (distance == -1 || dist < distance) {
-                    distance = dist;
-                    ancestor = cur;
-                }
+        for (int i : w) {
+            queue2.add(i);
+        }
+
+        int depth1 = 0, depth2 = 0;
+
+        int[] res = new int[]{Integer.MAX_VALUE, -1};
+        int remainingSteps = -1;
+        boolean firstTimeCovisited = false;
+        while ((!queue1.isEmpty() || !queue2.isEmpty()) && (remainingSteps == -1 || remainingSteps > 0)) {
+            if (remainingSteps > 0) {
+                remainingSteps--;
             }
-            for (int i : graph.adj(cur)) {
-                if (marked2[i]) {
+
+            // find adj of q1 elements.
+            Queue<Integer> tmpQ1 = new LinkedList<>();
+            while (!queue1.isEmpty()) {
+                int node = queue1.poll();
+                // consider the situation where 1->2, 1->3, 2->3, then while processing 2, 3 is added to open set again.
+                if (map1.containsKey(node)) {
                     continue;
                 }
-                q.enqueue(i);
-                marked2[i] = true;
-                distTo[i] = distTo[cur] + 1;
+
+                // co-visited a node
+                if (map2.containsKey(node)) {
+                    int newLength = depth1 + map2.get(node);
+                    if (newLength < res[0]) {
+                        res[0] = newLength;
+                        res[1] = node;
+                        if (!firstTimeCovisited) {
+                            remainingSteps = Integer.max(depth1, map2.get(node));
+                        }
+                        firstTimeCovisited = true;
+                    }
+                }
+                // prepare for next iteration
+                for (int vertex : graph.adj(node)) {
+                    if (!map1.containsKey(vertex)) {
+                        tmpQ1.add(vertex);
+                    }
+                }
+                map1.put(node, depth1);
             }
+            queue1 = tmpQ1;
+            depth1++;
+
+            // q2 elements;
+            Queue<Integer> tmpQ2 = new LinkedList<>();
+            while (!queue2.isEmpty()) {
+                int node = queue2.poll();
+                if (map2.containsKey(node)) {
+                    continue;
+                }
+
+                // co-visited a node
+                if (map1.containsKey(node)) {
+                    int newLength = map1.get(node) + depth2;
+                    if (newLength < res[0]) {
+                        res[0] = newLength;
+                        res[1] = node;
+                        if (!firstTimeCovisited) {
+                            remainingSteps = Integer.max(map1.get(node), depth2);
+                        }
+                        firstTimeCovisited = true;
+                    }
+                }
+                // prepare for next iteration
+                for (int vertex : graph.adj(node)) {
+                    if (!map2.containsKey(vertex)) {
+                        tmpQ2.add(vertex);
+                    }
+                }
+                map2.put(node, depth2);
+            }
+            queue2 = tmpQ2;
+            depth2++;
         }
-        return new int[]{distance, ancestor};
+        if (res[0] == Integer.MAX_VALUE) {
+            return new int[]{-1, -1};
+        } else {
+            return res;
+        }
     }
 
     // do unit testing of this class
@@ -142,5 +194,29 @@ public class SAP {
         int length = sap.length(v, w);
         int ancestor = sap.ancestor(v, w);
         StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+
+
+        BreadthFirstDirectedPaths a = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths b = new BreadthFirstDirectedPaths(G, w);
+
+        for (Integer integer : a.pathTo(60600)) {
+            System.out.print(" " + integer);
+        }
+        System.out.println();
+        for (Integer integer : b.pathTo(60600)) {
+            System.out.print(" " + integer);
+        }
+        System.out.println();
+
+
+        for (Integer integer : a.pathTo(81004)) {
+            System.out.print(" " + integer);
+        }
+        System.out.println();
+        for (Integer integer : b.pathTo(81004)) {
+            System.out.print(" " + integer);
+        }
+        System.out.println();
+
     }
 }
